53955f5ebf51bf2f98f9cdfcfe402a0461abe9e3
ZinixZay <romadesl21@gmail.com>
2022-12-13 23:06:32 +0300

    ray casting walls, textures, matrix map

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import pygame
+
+from entities.main_player import MainPlayer
 from settings import *
+from utils.drawing import Drawing
 
 pygame.init()
 screen = pygame.display.set_mode(SIZE_SCREEN)
+pygame.mouse.set_visible(False)
 clock = pygame.time.Clock()
+player = MainPlayer(speed=4)
+drawing = Drawing(screen)
 
 
 class RayCastingGame:
@@ -13,7 +19,11 @@ class RayCastingGame:
                 if event.type == pygame.QUIT:
                     exit()
 
+            player.movement()
             screen.fill(BLACK)
+
+            drawing.background()
+            drawing.world(player.pos, player.angle)
 
             pygame.display.flip()
             clock.tick(FPS)
