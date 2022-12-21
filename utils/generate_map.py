def50e6cbab3687f30ce7163ee2a7fb7bf5911f9
ZinixZay <romadesl21@gmail.com>
2022-12-21 19:08:34 +0300

    added field generation

--- utils/generate_map.py
+++ utils/generate_map.py
@@ -11,35 +11,45 @@ class MapGenerator:
         self.map = None
 
     def generate(self):
-        new_map = list()
+        new_map = [[0 for _ in range(self.width)] for _ in range(self.height)]
         for row in range(self.height):
-            new_row = []
             for column in range(self.width):
 
                 if row == 0 or row == (self.height - 1):
-                    new_map.append([self.__build_wall() for _ in range(self.width)])
+                    new_map[row] = [self.__generate_wall() for _ in range(self.width)]
                     break
 
                 if column == 0 or column == (self.width - 1):
-                    new_row.append(self.__build_wall())
+                    new_map[row][column] = self.__generate_wall()
                     continue
 
-                new_row.append(0)
+                new_map[row][column] = self.__generate_cell(row, column, new_map)
 
-            else:
-                new_map.append(new_row)
         self.map = np.asarray(new_map)
 
     @staticmethod
-    def __build_wall() -> int:
+    def __generate_wall() -> int:
         for potential_wall, chance in generate_rate.items():
             if chance >= random.randint(0, 100):
                 return 2
             else:
                 return 1
 
+    def __generate_cell(self, row, col, field):
+        c = 0
+        wall_chance = intensivity
+        neighbours = [[row + 1, col], [row, col + 1], [row - 1, col], [row, col - 1]]
+        for cell in neighbours:
+            if field[cell[0]][cell[1]] == 0:
+                c += 1
+        rang = 100 - c * 10
+        if wall_chance >= random.randint(0, rang):
+            return self.__generate_wall()
+        else:
+            return 0
 
-gr = MapGenerator(5, 20)
+
+gr = MapGenerator(10, 15)
 gr.generate()
 print(gr.map)
 
--- utils/generate_rate_config.py
+++ utils/generate_rate_config.py
@@ -2,3 +2,4 @@ default_wall = 'wall10.jpg'
 generate_rate = {
     'wall11.jpg': 10,
 }
+intensivity = 15
