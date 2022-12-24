ec4d82ed7b6236ad00f5cf4e38d5e3ec9929c5bf
ZinixZay <romadesl21@gmail.com>
2022-12-25 02:06:40 +0300

    improved generation

--- utils/generate_map.py
+++ utils/generate_map.py
@@ -7,6 +7,8 @@ class MapGenerator:
     def __init__(self, height: int, width: int) -> None:
         self.height, self.width = height, width
         self.map = None
+        self.space = list()
+        self.hero_spawn = None
 
     def generate(self):
         new_map = np.zeros((self.height, self.width), dtype=np.int32)
@@ -24,18 +26,61 @@ class MapGenerator:
                 new_map[row][column] = self.__generate_cell(row, column, new_map)
 
         self.map = new_map
+        self.__destroy_no_ways()
+        row, col = random.choice(self.space)
+        new_map[row][col] = 5
+        self.space.remove([row, col])
+        self.hero_spawn = [row, col]
 
     @staticmethod
     def __generate_wall() -> int:
         for potential_wall, chance in generate_rate.items():
             return 2 if chance >= random.randint(0, 100) else 1
 
+    @staticmethod
+    def __get_neighbours(row, col) -> list:
+        return [[row + 1, col], [row, col + 1], [row - 1, col], [row, col - 1]]
+
     def __generate_cell(self, row, col, field):
-        c = 0
+        walls_near = 0
         wall_chance = intensivity
-        neighbours = [[row + 1, col], [row, col + 1], [row - 1, col], [row, col - 1]]
+        neighbours = self.__get_neighbours(row, col)
         for cell in neighbours:
-            if field[cell[0]][cell[1]] == 0:
-                c += 1
-        rang = 100 - c * 10
-        return self.__generate_wall() if wall_chance >= random.randint(0, rang) else 0
+            if field[cell[0]][cell[1]] != 0:
+                walls_near += 1
+        match walls_near:
+            case 0:
+                rang = 100
+            case 1:
+                rang = 50
+            case 2:
+                rang = 50
+            case 3:
+                rang = 100
+            case 4:
+                rang = 0
+        if wall_chance >= random.randint(0, rang):
+            return self.__generate_wall()
+        else:
+            self.space.append([row, col])
+            return 0
+
+    def __destroy_no_ways(self):
+        for row in range(self.height):
+            for col in range(self.width):
+                if row == 0 or col == 0 or row == self.height - 1 or col == self.width - 1:
+                    continue
+                walls_near = 0
+                neighbours = self.__get_neighbours(row, col)
+                for cell in neighbours:
+                    if self.map[cell[0]][cell[1]] != 0:
+                        walls_near += 1
+                match walls_near:
+                    case 0:
+                        self.map[row][col] = 0
+                    case 4:
+                        self.map[row][col] = 1
+                    case 3:
+                        self.map[row][col] = 1
+
+
--- utils/test_utils/generate_map_test.py
+++ utils/test_utils/generate_map_test.py
@@ -3,11 +3,17 @@ from utils.generate_map import MapGenerator
 
 def pretty_print_map(_map):
     for row in _map:
-        [print(' ' if col == 0 else '█', end='') for col in row]
+        for col in row:
+            if col == 0:
+                print(' ', end='')
+            elif col == 5:
+                print('@', end='')
+            else:
+                print('█', end='')
         print()
 
 
-gr = MapGenerator(10, 15)
+gr = MapGenerator(16, 24)
 gr.generate()
 print('TEST GENERATE 10*15')
 pretty_print_map(gr.map)
