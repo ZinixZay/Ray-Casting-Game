e723d1f2efcc8dc236774fe583618acff684d7f5
dmitrii1011sg <dmitrii1011sg@gmail.com>
2022-12-26 22:51:43 +0300

    fix

--- utils/drawing.py
+++ utils/drawing.py
@@ -10,9 +10,9 @@ class Drawing:
         self.font = pygame.font.SysFont('Arial', 36, bold=True)
         self.textures = {
             1: pygame.image.load(IMAGES_PATH+'\\walls\\wall1.png').convert(),
-            2: pygame.image.load(IMAGES_PATH+'\\walls\\wall2.png').convert(),
+            2: pygame.image.load(IMAGES_PATH + '\\walls\\wall2.png').convert(),
             'S': pygame.image.load(IMAGES_PATH + '\\sky\\sky1.png').convert(),
-            'error_texture': pygame.image.load(IMAGES_PATH+'\\walls\\wall0.png').convert(),
+            'error_texture': pygame.image.load(IMAGES_PATH+'\\walls\\wall1.png').convert(),
         }
 
     def draw_floor_sky(self, angle: float) -> None:
