8947fa512a604ae6ab11cc420b134ad7078c0748
dmitrii1011sg <dmitrii1011sg@gmail.com>
2023-01-07 00:47:07 +0300

    title for menu, added new font

--- components/menu_start/menu_start.py
+++ components/menu_start/menu_start.py
@@ -1,11 +1,16 @@
+import pygame.font
+
 from components.button.button import Button
 from core.status_game import STATUS_GAME
+from paths import FONT_PATH
+from settings import RED, LIGHT_GRAY, HALF_HEIGHT
 
 
 class MenuStart:
     def __init__(self, btn_image, background_image):
         self.title = 'No Return'
         self.background_image = background_image
+        self.font = pygame.font.Font(FONT_PATH +'\\GULAG Pavljenko.otf', 100)
         self.buttons = {
             Button((600, 400), (300, 80), btn_image, 'Start'): STATUS_GAME.GAME_PROCESS,
             Button((600, 500), (300, 80), btn_image, 'Exit'): STATUS_GAME.EXIT,
@@ -13,6 +18,8 @@ class MenuStart:
 
     def draw(self, screen):
         screen.blit(self.background_image, (0, 0))
+        text = self.font.render(self.title, 0, LIGHT_GRAY)
+        screen.blit(text, (HALF_HEIGHT-20, 100))
         for btn in self.buttons:
             btn.draw(screen)
 
