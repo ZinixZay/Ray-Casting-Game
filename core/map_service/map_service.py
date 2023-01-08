dd03b631d32b6b666376874ce6d83cd180bf877a
dmitrii1011sg <dmitrii1011sg@gmail.com>
2023-01-08 19:03:42 +0300

    fix warning

--- core/map_service/map_service.py
+++ core/map_service/map_service.py
@@ -1,7 +1,7 @@
 import json
 import pygame
 from paths import *
-from numba import int32
+from numba import int64
 from numba.core import types
 from numba.typed import Dict
 from core.map_generator.map_generator import MapGenerator
@@ -13,7 +13,7 @@ class MapService:
         self.map_generator = MapGenerator(*WORLD_SIZE)
         self.matrix_map = list()
         self.collisions = list()
-        self.walls = Dict.empty(key_type=types.UniTuple(int32, 2), value_type=int32)
+        self.walls = Dict.empty(key_type=types.UniTuple(int64, 2), value_type=int64)
         self.start_player_pos = None
         self.entities = list()
         self.mini_map = set()
@@ -33,9 +33,9 @@ class MapService:
         self.reset_param()
 
     def reset_param(self) -> None:
-        self.walls = Dict.empty(key_type=types.UniTuple(int32, 2), value_type=int32)
-        self.collisions = list()
-        self.mini_map = set()
+        self.walls = Dict.empty(key_type=types.UniTuple(int64, 2), value_type=int64)
+        self.collisions.clear()
+        self.mini_map.clear()
         for j, row in enumerate(self.matrix_map):
             for i, char in enumerate(row):
                 if char:
