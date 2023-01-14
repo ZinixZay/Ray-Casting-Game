65499e33cef19e0662f85c13cac1930d237a43b0
ZinixZay <romadesl21@gmail.com>
2023-01-14 17:23:07 +0300

    data correction

--- core/data_service/data_service.py
+++ core/data_service/data_service.py
@@ -5,10 +5,13 @@ class DataService:
     def __init__(self):
         pass
 
-    def save_data(self, head: str, body: dict):
+    def save_data(self, head: str, body: dict) -> str:
         with open('C:/Users/Daniel/PycharmProjects/projects/Ray-Casting-Game/assets/data/data.json') as f:
             data = json.load(f)
+        if head in data.keys():
+            return 'error'
         data[head] = body
         dumped_data = json.dumps(data)
         with open('C:/Users/Daniel/PycharmProjects/projects/Ray-Casting-Game/assets/data/data.json', 'w') as f:
             f.write(dumped_data)
+        return 'ok'
