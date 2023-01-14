8866860cb42bbd938e034efd437b1b91af33f418
ZinixZay <romadesl21@gmail.com>
2023-01-14 17:21:33 +0300

    save data function

--- core/data_service/data_service.py
+++ core/data_service/data_service.py
@@ -1,3 +1,14 @@
+import json
+
+
 class DataService:
     def __init__(self):
         pass
+
+    def save_data(self, head: str, body: dict):
+        with open('C:/Users/Daniel/PycharmProjects/projects/Ray-Casting-Game/assets/data/data.json') as f:
+            data = json.load(f)
+        data[head] = body
+        dumped_data = json.dumps(data)
+        with open('C:/Users/Daniel/PycharmProjects/projects/Ray-Casting-Game/assets/data/data.json', 'w') as f:
+            f.write(dumped_data)
