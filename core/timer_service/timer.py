48805393566faac03fc7096b30a837d2a13e3d72
ZinixZay <romadesl21@gmail.com>
2023-01-15 22:06:17 +0300

    set timer

--- core/timer_service/timer.py
+++ core/timer_service/timer.py
@@ -1,6 +1,17 @@
 import time
+import threading
+
 
 class Timer:
     def __init__(self):
+        self.end_time = None
 
+    def set_timer(self, seconds: int):
+        self.end_time = round(time.time()) + seconds
+
+    def __reboot(self):
+        self.end_time = None
+
+    # def time_proc(self):
+    #     while self.end_time > round(time.time()):
 
