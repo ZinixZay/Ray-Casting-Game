f4ad624e2e07822210096e13e6d1a0b1a363c8b6
ZinixZay <romadesl21@gmail.com>
2023-01-15 22:09:14 +0300

    timer loop

--- core/timer_service/timer.py
+++ core/timer_service/timer.py
@@ -5,13 +5,20 @@ import threading
 class Timer:
     def __init__(self):
         self.end_time = None
+        self.cur_time = None
+        self.available = True
 
     def set_timer(self, seconds: int):
-        self.end_time = round(time.time()) + seconds
+        if self.available:
+            self.available = False
+            self.end_time = round(time.time()) + seconds
 
     def __reboot(self):
         self.end_time = None
+        self.cur_time = None
+        self.available = True
 
-    # def time_proc(self):
-    #     while self.end_time > round(time.time()):
-
+    def time_proc(self):
+        while self.end_time > round(time.time()):
+            self.cur_time = self.end_time - round(time.time())
+        self.__reboot()
