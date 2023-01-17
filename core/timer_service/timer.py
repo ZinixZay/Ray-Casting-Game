84efa1afd9ad0fd4578bcb8908cd1ef44a31bbc4
ZinixZay <romadesl21@gmail.com>
2023-01-17 16:53:40 +0300

    pep8 edit

--- core/timer_service/timer.py
+++ core/timer_service/timer.py
@@ -12,9 +12,11 @@ class Timer:
     def time_pars(self) -> str:
         if self.end_sec:
             m = self.end_sec - self.start_sec
-            return datetime.datetime(1, 1, 1, minute=int((m / (1000 * 60)) % 60), second=int((m / 1000) % 60)).strftime('%M:%S')
+            return datetime.datetime(1, 1, 1, minute=int(
+                (m / (1000 * 60)) % 60), second=int((m / 1000) % 60)).strftime('%M:%S')
         m = self.time.get_ticks() - self.start_sec
-        return datetime.datetime(1, 1, 1, minute=int((m / (1000 * 60)) % 60), second=int((m / 1000) % 60)).strftime('%M:%S')
+        return datetime.datetime(1, 1, 1, minute=int(
+            (m / (1000 * 60)) % 60), second=int((m / 1000) % 60)).strftime('%M:%S')
 
     def start_time(self) -> None:
         self.start_sec = self.time.get_ticks()
