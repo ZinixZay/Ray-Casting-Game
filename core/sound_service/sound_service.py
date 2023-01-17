c93fbe5f957feba4af933a9c6dfccc5ca3677c60
ZinixZay <romadesl21@gmail.com>
2023-01-17 17:21:29 +0300

    critical bug with sound service fixed

--- core/sound_service/sound_service.py
+++ core/sound_service/sound_service.py
@@ -24,6 +24,7 @@ class SoundService:
         self.death_sound = pygame.mixer.Sound(SOUND_PATH+'death.mp3')
 
         self.win_sound = pygame.mixer.Sound(SOUND_PATH+'win.mp3')
+        self.win_sound.set_volume(0.3)
 
         self.lose_sound = pygame.mixer.Sound(SOUND_PATH+'lose.mp3')
         self.lose_sound.set_volume(0.3)
@@ -43,8 +44,8 @@ class SoundService:
 
         self.shot_channel = pygame.mixer.Channel(3)
 
-        playlist_sound_game = [SOUND_PATH+'game1.mp3', SOUND_PATH+'game2.mp3', SOUND_PATH+'game3.mp3']
-        self.game_songs = [pygame.mixer.Sound(i) for i in playlist_sound_game]
+        self.playlist_sound_game = [SOUND_PATH+'game1.mp3', SOUND_PATH+'game2.mp3', SOUND_PATH+'game3.mp3']
+        self.game_songs = [pygame.mixer.Sound(i) for i in self.playlist_sound_game]
         for sound in self.game_songs:
             sound.set_volume(0.1)
 
@@ -61,10 +62,12 @@ class SoundService:
     def sound_win(self) -> None:
         self.game_sound_channel.stop()
         self.game_sound_channel.play(self.win_sound)
+        pygame.mixer.music.stop()
 
     def sound_lose(self) -> None:
         self.game_sound_channel.stop()
         self.game_sound_channel.play(self.lose_sound)
+        pygame.mixer.music.stop()
 
     def sound_start(self) -> None:
         pygame.mixer.music.stop()
@@ -72,7 +75,7 @@ class SoundService:
 
     def sound_steps(self, moving) -> None:
         if moving:
-            if not pygame.mixer.Channel(1).get_busy():
+            if not pygame.mixer.Channel(2).get_busy():
                 self.steps_channel.play(self.steps_sound)
         else:
             self.steps_channel.stop()
@@ -86,8 +89,18 @@ class SoundService:
 
     def sound_game(self) -> None:
         self.game_sound_channel.stop()
-        for i in range(6):
-            self.game_sound_channel.play(choice(self.game_songs))
+        playlist = self.playlist_sound_game.copy()
+        shuffle(playlist)
+        pygame.mixer.music.load(playlist[0])
+        playlist.pop(0)
+        pygame.mixer.music.play()
+        while len(playlist) != 0:
+            pygame.mixer.music.queue(playlist[0])
+            playlist.pop(0)
+        for song in self.playlist_sound_game:
+            pygame.mixer.music.queue(song)
+            pygame.mixer.music.set_volume(0.1)
+            pygame.mixer.music.play()
 
     @staticmethod
     def sound_pause() -> None:
